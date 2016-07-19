2a6340f673daeae923b7e5b40dc5ee2f5b36f47f
Brian Sayler <sayler.b@gmail.com>
2016-07-18 21:59:06 -0600

    change syntax in merchant request spec

--- spec/requests/merchant_request_spec.rb
+++ spec/requests/merchant_request_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Merchant record endpoint" do
 
-  let!(:merchant_1) { create(:merchant) }
-  let!(:merchant_2) { create(:merchant) }
-  let!(:merchant_3) { create(:merchant) }
-  let!(:merchant_4) { create(:merchant) }
+  let!(:merchant1) { create(:merchant) }
+  let!(:merchant2) { create(:merchant) }
+  let!(:merchant3) { create(:merchant) }
+  let!(:merchant4) { create(:merchant) }
 
   it "returns the information for a single merchant" do
     get "/api/v1/merchants.json"
@@ -16,29 +16,28 @@ RSpec.describe "Merchant record endpoint" do
     expect(data.length).to eq(4)
 
     expect(data.first.length).to eq(4)
-    expect(data.first[:id]).to eq(merchant_1.id) 
-    expect(data.first[:name]).to eq(merchant_1.name) 
+    expect(data.first[:id]).to eq(merchant1.id) 
+    expect(data.first[:name]).to eq(merchant1.name) 
     expect(data.first[:created_at]).to eq('2012-03-27T14:53:59.000Z') 
     expect(data.first[:updated_at]).to eq('2012-03-27T14:53:59.000Z') 
     
     expect(data.last.length).to eq(4)
-    expect(data.last[:id]).to eq(merchant_4.id) 
-    expect(data.last[:name]).to eq(merchant_4.name) 
+    expect(data.last[:id]).to eq(merchant4.id) 
+    expect(data.last[:name]).to eq(merchant4.name) 
     expect(data.last[:created_at]).to eq('2012-03-27T14:53:59.000Z') 
     expect(data.last[:updated_at]).to eq('2012-03-27T14:53:59.000Z') 
   end
 
   it "returns the information for a single merchant" do
-    merchant_1_id = merchant_1.id
-    get "/api/v1/merchants/#{merchant_1_id}.json"
+    get "/api/v1/merchants/#{merchant1.id}.json"
 
     data = JSON.parse(response.body, symbolize_names: :true )
 
     expect(response).to be_success
     expect(data.length).to eq(4)
    
-    expect(data[:id]).to eq(merchant_1.id) 
-    expect(data[:name]).to eq(merchant_1.name) 
+    expect(data[:id]).to eq(merchant1.id) 
+    expect(data[:name]).to eq(merchant1.name) 
     expect(data[:created_at]).to eq('2012-03-27T14:53:59.000Z') 
     expect(data[:updated_at]).to eq('2012-03-27T14:53:59.000Z') 
   end
