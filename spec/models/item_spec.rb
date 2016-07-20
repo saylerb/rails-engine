26ca93a22d94d5bfb7735f2190aa4b3824f147f1
Brian Sayler <sayler.b@gmail.com>
2016-07-19 19:16:46 -0600

    write item model spec and factory

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:item_instance) { build(:item) }
+
+  it "has a valid factory" do
+    expect(item_instance).to be_valid
+    expect(item_instance).to respond_to(:name)
+    expect(item_instance).to respond_to(:merchant)
+    expect(item_instance).to respond_to(:description)
+    expect(item_instance).to respond_to(:unit_price)
+    expect(item_instance).to respond_to(:created_at)
+    expect(item_instance).to respond_to(:updated_at)
+  end
+
+  describe "validates attributes" do
+    it { expect(item_instance).to validate_presence_of(:name) }
+    it { expect(item_instance).to validate_presence_of(:description) }
+    it { expect(item_instance).to validate_presence_of(:unit_price) }
+    it { expect(item_instance).to validate_presence_of(:created_at) }
+    it { expect(item_instance).to validate_presence_of(:updated_at) }
+  end
+
+
 end
