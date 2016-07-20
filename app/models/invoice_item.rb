3f1bf679d7cd4f3991a73789c6482b8760fdbc3a
Brian Sayler <sayler.b@gmail.com>
2016-07-19 22:42:47 -0600

    write InvoiceItem model spec, validations, and factory

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,11 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
+
+  validates :invoice, presence: :true
+  validates :item, presence: :true
+  validates :quantity, presence: :true
+  validates :unit_price, presence: :true
+  validates :created_at, presence: :true
+  validates :updated_at, presence: :true
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:invoice_item_instance) { build(:invoice_item) }
+
+  it "has a valid factory" do
+    expect(invoice_item_instance).to be_valid
+    expect(invoice_item_instance).to respond_to(:invoice)
+    expect(invoice_item_instance).to respond_to(:item)
+    expect(invoice_item_instance).to respond_to(:quantity)
+    expect(invoice_item_instance).to respond_to(:unit_price)
+    expect(invoice_item_instance).to respond_to(:created_at)
+    expect(invoice_item_instance).to respond_to(:updated_at)
+  end
+
+  describe "validates attributes" do
+    it { expect(invoice_item_instance).to validate_presence_of(:invoice) }
+    it { expect(invoice_item_instance).to validate_presence_of(:item) }
+    it { expect(invoice_item_instance).to validate_presence_of(:quantity) }
+    it { expect(invoice_item_instance).to validate_presence_of(:unit_price) }
+    it { expect(invoice_item_instance).to validate_presence_of(:created_at) }
+    it { expect(invoice_item_instance).to validate_presence_of(:updated_at) }
+  end
+
+  describe "has the correct associations" do
+    it { expect(invoice_item_instance).to belong_to(:invoice) }
+    it { expect(invoice_item_instance).to belong_to(:item) }
+  end
 end
